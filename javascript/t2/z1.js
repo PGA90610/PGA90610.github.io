2e06b8c7d38da916a57da4fd1c81f857bc28adee
PGA90610 <93533080+PGA90610@users.noreply.github.com>
2022-09-12 17:44:41 +0300

    Add files via upload

---
+++ javascript/t2/z1.js
@@ -0,0 +1,25 @@
+﻿/*let kovbasa = 100;
+let hlib = 70;
+let kava = 30
+console.log(`Загальна вартість кошика ${kovbasa+kava*hlib}`)*/
+let kilkist = 3;
+
+let towar = 304;
+let garantiya = 200;
+let dostavka = 40;
+let imya = 'Петро'
+let Airpots = 'Airpots 2'
+let garant_time = 1;
+let suma = towar*kilkist+garantiya*kilkist+dostavka;
+
+
+console.log(`Вітаю, Петро! Ваше замовлення:
+
+${Airpots} за ціною - ${towar} у кількості ${kilkist} штук
+Гарантія на ${garant_time} рік вартістю - ${garantiya} у кількості ${kilkist} штук
+
+Вартість доставки - ${dostavka}грн
+
+прийнято та буде оброблено протягом 24 години.
+
+Загальна вартість замовлення - ${suma}`)
